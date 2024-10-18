85e62c007f5a8432431746083b9c26a99782f8b9
Jean-Baptiste <ecrireajb@gmail.com>
2024-10-18 15:11:44 +0200

    Init Spring Boot project

---
+++ src/test/java/com/jeanbaptisteparis/potfolio_api/PotfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanbaptisteparis.potfolio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
