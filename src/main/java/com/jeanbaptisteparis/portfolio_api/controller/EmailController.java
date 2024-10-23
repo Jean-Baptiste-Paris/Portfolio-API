683ce3ae3822eaf8911ee97cff992b3bbd970148
Jean-Baptiste <ecrireajb@gmail.com>
2024-10-23 15:15:53 +0200

    Create EmailController

---
+++ src/main/java/com/jeanbaptisteparis/portfolio_api/controller/EmailController.java
@@ -0,0 +1,21 @@
+package com.jeanbaptisteparis.portfolio_api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeanbaptisteparis.portfolio_api.entity.Email;
+import com.jeanbaptisteparis.portfolio_api.service.EmailService;
+
+@RestController
+public class EmailController {
+    
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/contact")
+    public void sendEmail(@RequestBody Email email) {
+        emailService.saveEmail(email);
+    }
+}
